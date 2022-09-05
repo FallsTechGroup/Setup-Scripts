159d41802ba24ff6f69ca6533a00f8f79077fe67
Ryan T. M. Reiffenberger <rtr@sio.midco.net>
2022-09-05 17:24:33 -0500

    Updated Auto Installs
    Updated scripts to include automatic installation

--- universal.sh
+++ universal.sh
@@ -2,11 +2,19 @@
 yum install epel-release
 
 # Update Repositories and Update Local Packages
-yum update
+yum update -y
 yum install nano -y
+yum install wget -y
 yum install python-pip python -y
 yum install htop -y
 
 # Install Zabbix Agent (Does not configure)
 rpm -Uvh https://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-release-4.0-2.el7.noarch.rpm
-yum install zabbix zabbix-agent
+yum install zabbix zabbix-agent -y
+
+# Download Zabbix Setup File & Replace
+
+# Start Zabbix Agent
+#service zabbix-agent restart
+
+
