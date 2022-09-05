c7ef003680650a21368fc9b624b31f27e27e0a39
Ryan T. M. Reiffenberger <rtr@sio.midco.net>
2022-09-05 17:38:23 -0500

    Updated Zabbix Agent Installation Commands

--- universal.sh
+++ universal.sh
@@ -13,8 +13,9 @@ rpm -Uvh https://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-release-4.0-2.e
 yum install zabbix zabbix-agent -y
 
 # Download Zabbix Setup File & Replace
+cd /etc/zabbix
+rm zabbix_agentd.conf
+wget https://raw.githubusercontent.com/FallsTechGroup/Setup-Scripts/main/zabbix_agentd.conf
 
 # Start Zabbix Agent
 #service zabbix-agent restart
-
-
